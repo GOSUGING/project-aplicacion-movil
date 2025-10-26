33ba929fcfe323b19a87b597fb8ad2e9c9fb2094
BrunoChannels <bruno.esteban.coloma@gmail.com>
2025-10-25 23:58:23 -0300

    Add product 'Steam Deck RGB 250GB'

--- app/src/main/java/com/example/levelup/ui/screens/ProductsScreen.kt
+++ app/src/main/java/com/example/levelup/ui/screens/ProductsScreen.kt
@@ -165,7 +165,8 @@ private fun sampleProducts(): List<Product> {
         Product(12, "Notebook Victus Gamer", "Potencia y rendimiento", 800000.0, imageUrl = null, imageRes = R.drawable.victus, category = "consolas"),
         Product(13, "Audifonos Marshall Major IV", "Calidad para largas sesiones", 89990.0, imageUrl = null, imageRes = R.drawable.marshall_heardphones, category = "accesorios"),
         Product(14, "Nvidia RTX 3070 TI 8GB", "Potencia y rendimiento", 700000.0, imageUrl = null, imageRes = R.drawable.rtx3070, category = "graficas"),
-        Product(15, "Nvidia RTX 2060 6GB", "Potencia y rendimiento", 400000.0, imageUrl = null, imageRes = R.drawable.rtx2060, category = "graficas")
+        Product(15, "Nvidia RTX 2060 6GB", "Potencia y rendimiento", 400000.0, imageUrl = null, imageRes = R.drawable.rtx2060, category = "graficas"),
+        Product(16, "Steam Deck RGB 250GB", "Consola portátil", 299990.0, imageUrl = null, imageRes = R.drawable.SteamDeck, category = "consolas")
 
         // Añade más productos para otras categorías
     )
