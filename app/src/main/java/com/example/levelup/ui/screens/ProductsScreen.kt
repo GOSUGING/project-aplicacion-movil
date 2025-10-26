c09b888d5a5171281e86220133ff436ed3bfc258
BrunoChannels <bruno.esteban.coloma@gmail.com>
2025-10-25 23:34:46 -0300

    Add product 'PC GAMER ASUS ROG'

--- app/src/main/java/com/example/levelup/ui/screens/ProductsScreen.kt
+++ app/src/main/java/com/example/levelup/ui/screens/ProductsScreen.kt
@@ -157,7 +157,8 @@ private fun sampleProducts(): List<Product> {
         Product(4, "Silla Gamer Ergonómica", "Comodidad para largas sesiones", 149990.0, imageUrl = null, imageRes = R.drawable.silla_gamer, category = "accesorios"),
         Product(5, "Audifonos Gamer HyperX", "Calidad de audio superior", 89990.0, imageUrl = null, imageRes = R.drawable.hyperx_cloud, category = "accesorios"),
         Product(6, "Mouse Logitech", "Precisión y respuesta rápida", 69990.0, imageUrl = null, imageRes = R.drawable.mouse_logitech, category = "accesorios"),
-        Product(7, "Polera y Polerón Level Up", "Estilo y comodidad", 129990.0, imageUrl = null, imageRes = R.drawable.polera_gamer, category = "accesorios")
+        Product(7, "Polera y Polerón Level Up", "Estilo y comodidad", 129990.0, imageUrl = null, imageRes = R.drawable.polera_gamer, category = "accesorios"),
+        Product(8, "PC Gamer ASUS ROG", "Potencia y rendimiento", 1299990.0, imageUrl = null, imageRes = R.drawable.rogstrix, category = "consolas")
         // Añade más productos para otras categorías
     )
 }
